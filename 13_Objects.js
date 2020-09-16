c48dd1557d8d4deab0bb5abb61b445dd2df3906c
deepak <deepakjha6210@gmail.com>
2020-09-16 23:36:07 +0530

    Today i learned about object in javaScript

--- 13_Objects.js
+++ 13_Objects.js
@@ -30,9 +30,92 @@ console.log(entreeValue);
 console.log(drinkValue);
 
 //  Accessing Object Properties with Variable 
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "unitas"
+};
+var playerNumber = 16;
+var player = testObj[playerNumber];
 
+// Updating Object Properties 
+var ourDog = {
+    "name": "Jackie",
+    "leg's": 4,
+    "tails": 1,
+    "Friends": ["everything!"]
+};
+ourDog.name = "Happy Camper";
 
+// Add New Properties to an object 
+var ourDog = {
+    "name": "Jackie",
+    "leg's": 4,
+    "tails": 1,
+    "Friends": ["everything!"]
+};
+ourDog.bark = "bow-wow";
 
+// Delete Properties From an Object 
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "trails": 1,
+    "friends": ["everythings!"],
+    "barks": "bow-bow"
+};
+delete ourDog.bark;
+
+//  Using Objects For lookups 
+function phoneticLookup(val){
+    var result = ""
+
+    var lookup = {
+        "Deepak": "Sharma",
+        "Randeep": "Jangra",
+        "Mohit": "Kantiwal",
+        "Amit": "Bansal"
+    }
+    result = lookup[val];
+
+    return result;
+}
+console.log(phoneticLookup("Deepak"));
+
+// Testing Objects For Properties 
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp){
+    if(myObj.hasOwnProperty(checkProp)){
+        return myObj[checkProp];
+    }else {
+        return "Not Found"
+    }
+}
+console.log(checkObj("pet"));
+
+// Manipulating Complex Objects 
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "piano Man",
+        "release_year": 1973,
+        "formats":[
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "beau carnes",
+        "title": "Cereal Man"
+    }
+];
 
 
 
